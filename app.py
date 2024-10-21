b39984fdd0dbe91ea7173249bd16e333bdc603d3
walter siesquen <matias.siesquenabad@gmail.com>
2024-10-21 18:43:22 -0500

    Implementando funcion para iniciar sesion

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-saldo=float(input("INGRESE SU SALDO: "))
-deposito=float(input("INGRESE MONTO A DEPOSITAR: "))
+# saldo=float(input("INGRESE SU SALDO: "))
+# deposito=float(input("INGRESE MONTO A DEPOSITAR: "))
 
 def depositarDinero(saldo, deposito):
     monto_final = saldo+deposito
     return monto_final
 
-print(depositarDinero(saldo, deposito))
+# print(depositarDinero(saldo, deposito))
+
+def iniciarSesion():
+    usuario0="0123456789"
+    contraseña0="1234"
+    usuario=input("Ingrese usario: ")
+    contraseña=input("Ingrese contraseña: ")
+    if usuario!=usuario0 or contraseña!=contraseña0:
+        print("Datos Incorrectos")
+    else:
+        print("Ingreso exitoso")
+
+iniciarSesion()
