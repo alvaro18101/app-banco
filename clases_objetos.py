7fa739dbe945a795a441576027c4e3be7b623b50
walter siesquen <matias.siesquenabad@gmail.com>
2024-11-02 11:49:17 -0500

    Merge branch 'main' of https://github.com/alvaro18101/app-banco

---
+++ clases_objetos.py
@@ -0,0 +1,34 @@
+# class Vehiculo:
+#     def __init__(self, color, numero_llantas, numero_asientos):
+#         self.color= color
+#         self.numero_llantas= int(numero_llantas)
+#         self.numero_asientos= int(numero_asientos)
+#     def Movimiento():
+#         while True:
+#             print("1.AVANZAR 2.RETROCEDER 3.QUIETO")
+#             movimiento0=input("Indique el estado del vehiculo: ")
+#             try:
+#                 if movimiento0==1:
+#                     break    
+#             except:
+#                     pass
+#     print(Movimiento())ç
+
+
+
+
+           
+class persona:
+    def __init__(self, nombres, apellidos, edad):
+        self.nombres=nombres
+        self.apellidos=apellidos
+        self.edad=int(edad)
+    def __str__(self):
+        print(self.nombres)
+def hablar(argumento):
+    argumento=("Hola, Alvaro es una papita")
+    print("hola")    
+    print(argumento) 
+
+
+
